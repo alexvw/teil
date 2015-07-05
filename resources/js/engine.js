d5320306df6a762637178d0bf78e6c2f8081301b
avanderwoude <alex.vander.woude@gmail>
2015-07-05 19:15:34 -0400

    vaguely playable if you don't like having fun or competing

--- resources/js/engine.js
+++ resources/js/engine.js
@@ -4,10 +4,11 @@ game data object
 */
 
 //basic namespace for now, could sandbox this later if necessary
-teilEngine = function(width, height, players){
+teilEngine = function(width, height, players, MAX_STRENGTH){
 	//private
     var time = 0;
 	var players = players;
+	var MAX_STRENGTH = MAX_STRENGTH;
 	
 	//board object
 	var board = function(w,h){
@@ -20,7 +21,7 @@ teilEngine = function(width, height, players){
 		}
 		
 		for (var i=0;i<w*h; i++){
-			theTiles.push(false);
+			theTiles.push(null);
 		}
 		
 		var getTileByXY = function(x,y){
@@ -49,18 +50,25 @@ teilEngine = function(width, height, players){
 		var dropToken = function(x,y,p){
 			//check if space is open first
 			var alreadyThere = this.getTileByXY(x,y);
-			if (alreadyThere === false){
+			if (alreadyThere === null){
 				console.log("Dropping token at " + x +", "+y);
 				theTiles[toLinear(x,y)] = new token(x,y,p);
 				return true;
 			}else if (alreadyThere instanceof token){
-				console.log("There is already a token at " + x +", "+y);
+				//console.log("There is already a token at " + x +", "+y);
 				//is it my token?
 				if (alreadyThere.p === p){
 					//its my token, increase strength
-					console.log("Adding to token at " + x +", "+y);
-					alreadyThere.strength++;
-					return true;
+					if (alreadyThere.strength < MAX_STRENGTH){
+						console.log("Adding to token at " + x +", "+y);
+						alreadyThere.strength++;
+						return true;
+					}else{
+						console.log("Max strength at " + x +", "+y);
+						//alreadyThere.strength++;
+						return false;
+
+					}
 				}
 				else {
 					console.log("There is already an enemy token at " + x +", "+y);
@@ -72,13 +80,150 @@ teilEngine = function(width, height, players){
 			}
 		}
 		
+		//recursive swap function
+		var randomSwap = function(oldBoard, randomBoard){
+			//base case, fully randomized so return
+			if (oldBoard.length == 0){
+				return randomBoard;
+				}
+			else {
+				var where = Math.round(Math.random()*(oldBoard.length-1));
+				var couldBeAToken = oldBoard.splice(where,1)[0];
+				if (couldBeAToken != null)
+					randomBoard.push(couldBeAToken);
+				randomSwap(oldBoard, randomBoard);
+			}
+		}
+		
+		var sim = function(step){
+			
+			var boardCopy = this.getTheTiles.slice(0);
+			//randomly shuffle tiles
+			var randomArray = new Array();
+			
+			randomSwap(boardCopy,randomArray);
+			
+			//for each of them, execute THE ALGORITHM on the board
+			for (var i=0;i<randomArray.length;i++){
+				var randomToken = randomArray[i];
+				var tX = randomToken.x;
+				var tY = randomToken.y;
+				
+				var origToken = this.getTileByXY(tX,tY);
+				algorithm(this,origToken);
+			}
+			
+		}
+		
+		//THE ALGORITHM
+		//does its magic on the original objects
+		var algorithm = function(board, token){
+			
+			var tX = token.x;
+			var tY = token.y;
+			var tP = token.p;
+			var tiles = board.getTheTiles;
+			
+			var polandObject = getPolandObject(board,token);
+			
+			if (polandObject.enemyCount){
+				//LET LOOSE THE DOGS OF WAR
+				
+			}
+			
+			
+		}
+		
+		var getPolandObject = function(board, token){
+			var tX = token.x;
+			var tY = token.y;
+			var tP = token.p;
+			var tiles = board.getTheTiles;
+			
+			/*
+			[0,1,2
+			 3,4,5
+			 6,7,8]
+			*/
+		
+			var neighborArray = [tiles[((tY-2)*w)+(tX-2)],
+								 tiles[((tY-2)*w)+(tX-1)],
+								 tiles[((tY-2)*w)+(tX)],
+								 tiles[((tY-1)*w)+(tX-2)],
+								 tiles[((tY-1)*w)+(tX-1)],
+								 tiles[((tY-1)*w)+(tX)],
+								 tiles[((tY)*w)+(tX-2)],
+								 tiles[((tY)*w)+(tX-1)],
+								 tiles[((tY)*w)+(tX)]];
+								 
+			//because math, override some ends
+			if (tX >= board.boardWidth){
+				neighborArray[2] = undefined;
+				neighborArray[5] = undefined;
+				neighborArray[8] = undefined;
+				}
+			else if (tX <= 1){
+				neighborArray[0] = undefined;
+				neighborArray[3] = undefined;
+				neighborArray[6] = undefined;
+				}
+				
+			if (tY >= board.boardHeight){
+				neighborArray[6] = undefined;
+				neighborArray[7] = undefined;
+				neighborArray[8] = undefined;
+				}
+			else if (tY <= 1){
+				neighborArray[0] = undefined;
+				neighborArray[1] = undefined;
+				neighborArray[2] = undefined;
+				}
+				
+			//of just looking to making a little bit of moneys
+			var polandArray = [];
+			var friendlyCount = 0;
+			var enemyCount = 0;
+			
+			for (var i=0;i<neighborArray.length;i++){
+				var status = 0;
+				if (typeof neighborArray[i] !== "undefined"){
+					status++;
+					if (neighborArray[i] != null){
+						status++;
+						if (neighborArray[i].p === tP){
+							status++;
+							friendlyCount++;
+						}
+						else{
+							enemyCount++;
+						}
+					}
+				}
+				polandArray.push(status);
+			}
+			
+			/*{
+			enemyCount:0,
+			friendlyCount:0,
+			polandArray:[]
+			}
+			
+			*/
+			return {enemyCount, friendlyCount, polandArray};
+		}
+		
+		var fight = function(polandObject){
+		
+		}
+		
 		return {
 			getTileByXY: getTileByXY,
 			getTheTiles: theTiles,
 			dropToken: dropToken,
 			createPlayer: createPlayer,
 			boardWidth: boardWidth,
-			boardHeight: boardHeight
+			boardHeight: boardHeight,
+			sim: sim
 		}
 	}
 	
--- resources/js/run.js
+++ resources/js/run.js
@@ -1,27 +1,32 @@
 //execution scripts
 
-
-
-
-/*function InvSqrt(float x){
-	   var xhalf = 0.5f * x;
-	   var i = *(int*)&x; // store floating-point bits in integer
-	   i = 0x5f3759d5 - (i >> 1); // initial guess for Newton's method
-	   x = *(float*)&i; // convert new bits into float
-	   x = x*(1.5f - xhalf*x*x); // One round of Newton's method
+	
+	var MAX_WIDTH = 768;
+	var MAX_HEIGHT = 768;
+	
+	var MAX_STRENGTH = 3;
+	
+	var player1;
+	var player2;
+	var running = true;
+	
+	var step = 0;
+	
+	/*function InvSqrt(x){
+	   var xhalf = 0.5 * x;
+	   //var i = *(int*)&x;
+	   i = 0x5f3759d5 - (i >> 1); 
+	   //x = *(float*)&i;
+	   x = x*(1.5 - xhalf*x*x); //Newton's method
 	   return x;
 	}*/
-	var MAX_WIDTH = 720;
-	var MAX_HEIGHT = 720;
-	
-	var ctx, screenWidth, screenHeight, board, size;
-		
+
 	function pageInit(te){
 		var width = $(window).width()-5;
 		var height = $(window).height()-5;
-		if (width>MAX_WIDTH)
+		//if (width>MAX_WIDTH)
 			screenWidth = MAX_WIDTH;
-		if (height>MAX_HEIGHT)
+		//if (height>MAX_HEIGHT)
 			screenHeight = MAX_HEIGHT;
 		
 		c = document.getElementById('c');
@@ -37,9 +42,12 @@
 		CANVAS_WIDTH = $('#c').width();
 		CANVAS_HEIGHT = $('#c').height();
 		
-		
-		
-		requestAnimFrame(render);
+		(function animloop(){
+			if (running){
+				requestAnimFrame(animloop);
+				render(ctx, screenWidth, screenHeight, te.getBoard);
+			}
+		})();
 	}
 	
 	//requestAnim shim layer by Paul Irish
@@ -56,11 +64,13 @@
 			};
 	})();
 
-	function render(){
+	function render(ctx, screenWidth, screenHeight, board){
 		//cache board
 		var renderBoard = board.getTheTiles;
 		
 		size = screenWidth/board.boardWidth;
+		
+		ctx.clearRect(0,0,screenWidth,screenHeight);
 		
 		//used only here
 		var boardToXY = function(bx,by){
@@ -76,14 +86,13 @@
 		for (i=0;i<renderBoard.length;i++){
 			var currentTile = renderBoard[i];
 			
-			if (currentTile === false){
+			if (currentTile === null){
 				//no token here
 			}else if (typeof currentTile.x !== "undefined"){
 				//found one
 				var sc = boardToXY(currentTile.x, currentTile.y);
-				
 				ctx.fillStyle = currentTile.p.color;
-				ctx.globalAlpha=(0.33*currentTile.strength);
+				ctx.globalAlpha=((0.8/MAX_STRENGTH)*currentTile.strength)+0.2;
 				ctx.fillRect(sc.x-(size), sc.y-(size), size, size);
 				ctx.globalAlpha=1;
 			}else{
@@ -91,13 +100,11 @@
 			}
 			
 		}
-			//if empty
-			//if tile, draw color of player
 	}
 	
 	var xyToBoard = function(x,y){
-		var boardx = Math.floor((x/(screenWidth/board.boardWidth)));
-		var boardy = Math.floor((y/(screenHeight/board.boardHeight)));
+		var boardx = Math.floor((x/(screenWidth/board.boardWidth))) + 1;
+		var boardy = Math.floor((y/(screenHeight/board.boardHeight))) + 1;
 		return {
 		bx:boardx,
 		by:boardy
@@ -106,36 +113,56 @@
 	
 	function goClick(x,y){
 		var ret = xyToBoard(x,y)
-		alert(ret.bx +", "+ret.by);
+		//alert(ret.bx +", "+ret.by);
+		var p = player2;
+		if (step%2)
+			p = player1;
+		var success = te.getBoard.dropToken(ret.bx,ret.by,p);
+		if (success)
+			step++;
+			
+		updateUI(step);
+		te.getBoard.sim(step);
 	}
 
+	function updateUI(step){
+		if (step % 2){
+			$('#p1').addClass("active");
+			$('#p2').removeClass("active");
+		}else{
+			$('#p2').addClass("active");
+			$('#p1').removeClass("active");
+		}
+	}
+	
+	
 	function start(){
 		var width = 16;
 		var height = 16;
 
-		window.te = new teilEngine(width,height,2);
+		window.te = new teilEngine(width,height,2,3);
 		board = te.getBoard;
-		var player1 = te.getBoard.createPlayer("Hero", "#c00");
-		var player2 = te.getBoard.createPlayer("Enemy", "#00c");
+		player1 = te.getBoard.createPlayer("Hero", "#c00");
+		player2 = te.getBoard.createPlayer("Enemy", "#00c");
 		
 		var x = 0;
 		var conflict = false;
 		
 		pageInit(te);
 		
-		while (x<300){
+		/*while (x<300){
 			var a = Math.round(1+Math.random()*(width-1));
 			var b = Math.round(1+Math.random()*(height-1));
-			var c = player1;
-			conflict = te.getBoard.dropToken(a,b,c);
+			var p = player1;
+			success = te.getBoard.dropToken(a,b,p);
 			
 			a = Math.round(1+Math.random()*(width-1));
 			b = Math.round(1+Math.random()*(height-1));
-			c = player2
-			conflict = te.getBoard.dropToken(a,b,c);
+			p = player2
+			success = te.getBoard.dropToken(a,b,p);
 			
 			x++;
-		}
+		}*/
 		
 		
 		//clickhandlers
